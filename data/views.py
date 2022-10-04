0aa155d032869362a58ab087f028e00df8051f91
Yasir Idriz <yasiridriz@gmail.com>
2022-10-04 12:00:25 +0200

    filter by type

--- data/views.py
+++ data/views.py
@@ -13,6 +13,10 @@ import csv
 
 @api_view(['GET'])
 def get_data(request): 
+    typeq = request.query_params.get("type")
+    yearq = request.query_params.get("year")
+    coordinatesq = request.query_params.get("coordinates")
+
     sm = pandas.read_csv('resources/nakamura_1979_sm_locations.csv')
     ai = pandas.read_csv('resources/nakamura_1983_ai_locations.csv')
     dm = pandas.read_csv('resources/nakamura_2005_dm_locations.csv')
@@ -24,12 +28,26 @@ def get_data(request):
     sml = sml[~sml.isin([np.nan, np.inf, -np.inf]).any(1)]
     ail = ail[~ail.isin([np.nan, np.inf, -np.inf]).any(1)]
     dml = dml[~dml.isin([np.nan, np.inf, -np.inf]).any(1)]
-        
 
-    return Response({
+    res = {
         "SM": sml,
         "DM": dml,
         "AI": ail
-    })
+    }  
+    
+    if typeq:
+        if typeq == "shallow":
+            del res["DM"]
+            del res["AI"]
+        if typeq == "deep":
+            del res["SM"]
+            del res["AI"]
+        if typeq == "artificial":
+            del res["SM"]
+            del res["DM"]
+      
+        
+
+    return Response(res)
 
 
